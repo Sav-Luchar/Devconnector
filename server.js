100730e09aeebbe2d74c5d5a62eeb470c4a7a4d9
Lulu-Luchar <chillhouette@gmail.com>
2019-09-14 11:33:38 -0700

    User model

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
+const users = require ('./routes/api/users');
+const profile = require ('./routes/api/profile');
+const posts = require ('./routes/api/posts');
+const bodyParser = require('body-parser');
 const app = express ();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 //DB config
 const db = require ('./config/keys').mongoURI
 //CONNECT to mongoDB
@@ -11,6 +18,10 @@ mongoose
     .catch(err => console.log (err));
 //First Route
 app.get('/', (req,res) => res.send('hello Empress Lulu'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = 5000;
 app.listen(port,()=> console.log(`Server running on port ${port}`));
